72659086ee7880a6e306b3167fa1f83a3f775415
mihoy3rd <1138745761@qq.com>
2025-05-22 06:19:52 +0800

    Elysia

--- kernel/kpm/super_access.c
+++ kernel/kpm/super_access.c
@@ -180,18 +180,21 @@ DYNAMIC_STRUCT_BEGIN(task_struct)
 #ifdef CONFIG_CGROUPS
     DEFINE_MEMBER(task_struct, cgroups)
 #endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0) 
 #ifdef CONFIG_SECURITY
     DEFINE_MEMBER(task_struct, security)
 #endif
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 0) 
     DEFINE_MEMBER(task_struct, thread)
+#endif
 DYNAMIC_STRUCT_END(task_struct)
 
 // =====================================================================================================================
 
 #define STRUCT_INFO(name) &(name##_info)
 
-static
-struct DynamicStructInfo* dynamic_struct_infos[] = {
+static struct DynamicStructInfo* dynamic_struct_infos[] = {
     STRUCT_INFO(mount),
     STRUCT_INFO(vfsmount),
     STRUCT_INFO(mnt_namespace),
