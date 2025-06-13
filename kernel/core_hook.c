SukiSU-Ultra/SukiSU-Ultra

commit b443b11df94f9e4248691267a5b29f8ad82adcb1

kernel: Refactoring the mount point unloading logic

kernel/core_hook.c
@@ -1052,14 +1052,6 @@ void susfs_try_umount_all(uid_t uid) {
 	ksu_try_umount("/data/adb/kpm", false, MNT_DETACH, uid);
 	/* For both Legacy KSU and Magic Mount KSU */
 	ksu_try_umount("/debug_ramdisk", true, MNT_DETACH, uid);
-	ksu_try_umount("/sbin", false, MNT_DETACH, uid);
-	
-	// try umount hosts file
-	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH, uid);
-
-	// try umount lsposed dex2oat bins
-	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH, uid);
-	ksu_try_umount("/apex/com.android.art/bin/dex2oat32", false, MNT_DETACH, uid);
 }
 #endif
 
@@ -1140,18 +1132,24 @@ out_ksu_try_umount:
 	// susfs come first, and lastly umount by ksu, make sure umount in reversed order
 	susfs_try_umount_all(new_uid.val);
 #else
-
 	// fixme: use `collect_mounts` and `iterate_mount` to iterate all mountpoint and
 	// filter the mountpoint whose target is `/data/adb`
-	ksu_try_umount("/system", true, 0);
-	ksu_try_umount("/system_ext", true, 0);
-	ksu_try_umount("/vendor", true, 0);
-	ksu_try_umount("/product", true, 0);
-	ksu_try_umount("/data/adb/modules", false, MNT_DETACH);
-	ksu_try_umount("/data/adb/kpm", false, MNT_DETACH);
+	try_umount("/system", true, 0);
+	try_umount("/vendor", true, 0);
+	try_umount("/product", true, 0);
+	try_umount("/system_ext", true, 0);
+
+	// try umount modules path
+	try_umount("/data/adb/modules", false, MNT_DETACH);
+
+	// try umount kpm path
+	try_umount("/data/adb/kpm", false, MNT_DETACH);
 
 	// try umount ksu temp path
-	ksu_try_umount("/debug_ramdisk", false, MNT_DETACH);
+	try_umount("/debug_ramdisk", false, MNT_DETACH);
+	
+	// try umount ksu su path
+	try_umount("/sbin", false, MNT_DETACH);
 	
 	// try umount hosts file
 	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH);

commit 4f01cff69fea7a16b8d8e3da3b49a46346d64793

kernel: core_hook: Fix and complete try_umount renaming and usage
Fixed function definition and restored missing ksu_try_umount calls that were unintentionally removed in previous commits. This includes unmounts for:

- /debug_ramdisk
- /sbin
- /system/etc/hosts
- /apex/com.android.art/bin/dex2oat64
- /apex/com.android.art/bin/dex2oat32

Fixes: b443b11df94f9e4248691267a5b29f8ad82adcb1 ("kernel: Refactoring the mount point unloading logic")
Fixes: da7632470d40f0629d902e97804817561cb05514 ("kernel: 使用 try_umount 替换 ksu_try_umount 以统一卸载逻辑")

Signed-off-by: schqiushui <orochi9999@gmail.com>

kernel/core_hook.c
@@ -1028,7 +1028,7 @@ static void ksu_sys_umount(const char *mnt, int flags)
 #ifdef CONFIG_KSU_SUSFS_TRY_UMOUNT
 void ksu_try_umount(const char *mnt, bool check_mnt, int flags, uid_t uid)
 #else
-static void ksu_try_umount(const char *mnt, bool check_mnt, int flags)
+static void try_umount(const char *mnt, bool check_mnt, int flags)
 #endif
 {
 	struct path path;
@@ -1075,6 +1075,14 @@ void susfs_try_umount_all(uid_t uid) {
 	ksu_try_umount("/data/adb/kpm", false, MNT_DETACH, uid);
 	/* For both Legacy KSU and Magic Mount KSU */
 	ksu_try_umount("/debug_ramdisk", true, MNT_DETACH, uid);
+	ksu_try_umount("/sbin", false, MNT_DETACH, uid);
+	
+	// try umount hosts file
+	ksu_try_umount("/system/etc/hosts", false, MNT_DETACH, uid);
+
+	// try umount lsposed dex2oat bins
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat64", false, MNT_DETACH, uid);
+	ksu_try_umount("/apex/com.android.art/bin/dex2oat32", false, MNT_DETACH, uid);
 }
 #endif
 