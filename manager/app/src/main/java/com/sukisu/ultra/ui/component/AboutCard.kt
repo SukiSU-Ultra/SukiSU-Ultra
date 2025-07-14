9d8a60fccbaeaacf426ae57b5de3ae4f177a7d34
MiRinChan <148533509+MiRinChan@users.noreply.github.com>
2025-07-14 11:30:48 +0800

    string: add copyright infomation

--- manager/app/src/main/java/com/sukisu/ultra/ui/component/AboutCard.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/component/AboutCard.kt
@@ -99,7 +99,10 @@ private fun AboutCardContent() {
                     htmlString = stringResource(
                         id = R.string.about_source_code,
                         "<b><a href=\"https://github.com/ShirkNeko/SukiSU-Ultra\">GitHub</a></b>",
-                        "<b><a href=\"https://t.me/SukiKSU\">Telegram</a></b>"
+                        "<b><a href=\"https://t.me/SukiKSU\">Telegram</a></b>",
+                        "<b>怡子曰曰</b>",
+                        "<b>明风 OuO</b>",
+                        "<b><a href=\"https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode.txt\">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International</a></b>"
                     ),
                     linkStyles = TextLinkStyles(
                         style = SpanStyle(
