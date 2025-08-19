2a4b54ab8d13b5f0cac5a50f42a94b7202f1f37f
mihoy3rd <1138745761@qq.com>
2025-08-19 09:57:04 +0800

    Revert "kermel: Refactoring and separating dynamic signature-related structures and functions and implement hot-signature updates"
    This reverts commit a9e3c1cc8f410a20ba9f6fd3a1d2e34804355316.
    
    Why would I revert this?
    Because it will cause some nongki kernel panic.

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -46,6 +46,7 @@
 #include "manager.h"
 #include "selinux/selinux.h"
 #include "throne_tracker.h"
+#include "throne_tracker.h"
 #include "kernel_compat.h"
 #include "kpm/kpm.h"
 #include "include/ksu_creds.h"
@@ -423,6 +424,7 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 				on_post_fs_data();
 				// Initializing Dynamic Signatures
         		ksu_dynamic_sign_init();
+        		ksu_load_dynamic_sign();
         		pr_info("Dynamic sign config loaded during post-fs-data\n");
 			}
 			break;
