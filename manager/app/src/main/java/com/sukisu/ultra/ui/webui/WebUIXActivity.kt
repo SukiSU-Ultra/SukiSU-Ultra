741e4e3bc3335bef2ad7addb0c4c25c04834ae4c
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2025-08-20 17:46:54 +0200

    manager: bump WebUI X

--- manager/app/src/main/java/com/sukisu/ultra/ui/webui/WebUIXActivity.kt
+++ manager/app/src/main/java/com/sukisu/ultra/ui/webui/WebUIXActivity.kt
@@ -1,112 +1,119 @@
 package com.sukisu.ultra.ui.webui
 
-import android.app.ActivityManager
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Build
-import android.os.Bundle
-import android.webkit.WebView
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.lifecycleScope
+import androidx.compose.material3.ColorScheme
 import com.dergoogler.mmrl.platform.Platform
-import com.dergoogler.mmrl.platform.model.ModId
-import com.dergoogler.mmrl.ui.component.Loading
-import com.dergoogler.mmrl.webui.screen.WebUIScreen
-import com.dergoogler.mmrl.webui.util.rememberWebUIOptions
+import com.dergoogler.mmrl.platform.PlatformManager
+import com.dergoogler.mmrl.ui.component.dialog.ConfirmData
+import com.dergoogler.mmrl.ui.component.dialog.confirm
+import com.dergoogler.mmrl.webui.activity.WXActivity
+import com.dergoogler.mmrl.webui.util.WebUIOptions
+import com.dergoogler.mmrl.webui.view.WebUIXView
 import com.sukisu.ultra.BuildConfig
-import com.sukisu.ultra.ui.theme.KernelSUTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.sukisu.ultra.ui.theme.ThemeConfig
+import com.sukisu.ultra.ui.theme._isSystemInDarkTheme
+import com.sukisu.ultra.ui.theme.createColorScheme
+import kotlinx.coroutines.CoroutineScope
+import kotlin.jvm.java
 
-class WebUIXActivity : ComponentActivity() {
-    private lateinit var webView: WebView
-
+class WebUIXActivity : WXActivity() {
     private val userAgent
         get(): String {
             val ksuVersion = BuildConfig.VERSION_CODE
 
-            val platform = Platform.get("Unknown") {
-                platform.name
+            val platform = PlatformManager.get(Platform.Unknown) {
+                platform
             }
 
-            val platformVersion = Platform.get(-1) {
+            val platformVersion = PlatformManager.get(-1) {
                 moduleManager.versionCode
             }
 
             val osVersion = Build.VERSION.RELEASE
             val deviceModel = Build.MODEL
 
-            return "SukiSU-Ultra /$ksuVersion (Linux; Android $osVersion; $deviceModel; $platform/$platformVersion)"
+            return "SukiSU-Ultra/$ksuVersion (Linux; Android $osVersion; $deviceModel; ${platform.name}/$platformVersion)"
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
-        webView = WebView(this)
+    val prefs: SharedPreferences get() = getSharedPreferences("settings", MODE_PRIVATE)
+    val context: Context get() = this
 
-        lifecycleScope.launch {
-            initPlatform()
+    override suspend fun onRender(scope: CoroutineScope) {
+        scope.initPlatform(context)
+        super.onRender(scope)
+
+        val darkTheme = when (ThemeConfig.forceDarkMode) {
+            true -> true
+            false -> false
+            null -> _isSystemInDarkTheme(context)
         }
 
-        val moduleId = intent.getStringExtra("id")!!
-        val name = intent.getStringExtra("name")!!
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            @Suppress("DEPRECATION")
-            setTaskDescription(ActivityManager.TaskDescription("SukiSU-Ultra - $name"))
-        } else {
-            val taskDescription =
-                ActivityManager.TaskDescription.Builder().setLabel("SukiSU-Ultra - $name").build()
-            setTaskDescription(taskDescription)
+        val colorScheme = createColorScheme(
+            context = context,
+            darkTheme = darkTheme
+        )
+
+        val loading = createLoadingRenderer(colorScheme)
+        setContentView(loading)
+
+        val ready = scope.initPlatform(context)
+
+        if (!ready.await()) {
+            confirm(
+                ConfirmData(
+                    title = "Failed!",
+                    description = "Failed to initialize platform. Please try again.",
+                    confirmText = "Close",
+                    onConfirm = {
+                        finish()
+                    },
+                ),
+                colorScheme = colorScheme
+            )
+            return
         }
 
-        val prefs = getSharedPreferences("settings", MODE_PRIVATE)
+        init(
+            darkTheme = darkTheme,
+            colorScheme = colorScheme
+        )
+    }
 
-        setContent {
-            KernelSUTheme {
-                var isLoading by remember { mutableStateOf(true) }
+    private fun init(darkTheme: Boolean, colorScheme: ColorScheme) {
+        val modId =
+            this.modId
+                ?: throw IllegalArgumentException("modId cannot be null or empty")
 
-                LaunchedEffect(Platform.isAlive) {
-                    while (!Platform.isAlive) {
-                        delay(1000)
-                    }
+        val webDebugging = prefs.getBoolean("enable_web_debugging", false)
+        val erudaInject = prefs.getBoolean("use_webuix_eruda", false)
 
-                    isLoading = false
-                }
+        val options = WebUIOptions(
+            modId = modId,
+            context = context,
+            debug = webDebugging,
+            isDarkMode = darkTheme,
+            // keep plugins disabled for security reasons
+            pluginsEnabled = false,
+            enableEruda = erudaInject,
+            cls = WebUIXActivity::class.java,
+            userAgentString = userAgent,
+            colorScheme = colorScheme
+        )
 
-                if (isLoading) {
-                    Loading()
-                    return@KernelSUTheme
-                }
+        this.view = WebUIXView(options).apply {
+            wx.addJavascriptInterface<WebViewInterface>()
+        }
 
-                val webDebugging = prefs.getBoolean("enable_web_debugging", false)
-                val erudaInject = prefs.getBoolean("use_webuix_eruda", false)
-                val dark = isSystemInDarkTheme()
-
-                val options = rememberWebUIOptions(
-                    modId = ModId(moduleId),
-                    debug = webDebugging,
-                    appVersionCode = BuildConfig.VERSION_CODE,
-                    isDarkMode = dark,
-                    enableEruda = erudaInject,
-                    cls = WebUIXActivity::class.java,
-                    userAgentString = userAgent
-                )
-
-                WebUIScreen(
-                    webView = webView,
-                    options = options,
-                    interfaces = listOf(
-                        WebViewInterface.factory()
-                    )
-                )
+        // Activity Title
+        config {
+            if (title != null) {
+                setActivityTitle("SukiSU-Ultra - $title")
             }
         }
+
+        setContentView(view)
     }
 }
