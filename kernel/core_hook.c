a7fb38a6b5b86f4486e6a981a35515114dbdd632
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-11-01 01:08:39 +0800

    ci: use ddk for faster ci, manual gki image build
    Co-authored-by: Ylarod <me@ylarod.cn>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -386,8 +386,8 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 		     unsigned long arg4, unsigned long arg5)
 {
 	// if success, we modify the arg5 as result!
-	u32 *result = (u32 *)arg5;
-	u32 reply_ok = KERNEL_SU_OPTION;
+	__maybe_unused u32 *result = (u32 *)arg5;
+	__maybe_unused u32 reply_ok = KERNEL_SU_OPTION;
 
 	if (likely(ksu_is_current_proc_umounted()))
 		return 0; // prevent side channel attack in ksu side
