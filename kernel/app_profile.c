SukiSU-Ultra/SukiSU-Ultra

commit d00c156181f044a494cd58c6a9ce3fa6ce2e35b5

kernel: use rcu to access allowlist, refactor get allowlist api (#3093)

kernel/app_profile.c
@@ -88,7 +88,7 @@ void setup_groups(struct root_profile *profile, struct cred *cred)
 static void do_disable_seccomp(void)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	struct task_struct *fake;
 	fake = kmalloc(sizeof(*fake), GFP_ATOMIC);
 	if (!fake) {
@@ -109,24 +109,24 @@ static void do_disable_seccomp(void)
 #endif
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	memcpy(fake, current, sizeof(*fake));
 #endif
 	current->seccomp.mode = 0;
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0) &&                           \
-     !defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 !defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	// put_seccomp_filter is allowed while we holding sighand
 	put_seccomp_filter(current);
 #endif
 	current->seccomp.filter = NULL;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
 	atomic_set(&current->seccomp.filter_count, 0);
 #endif
 	spin_unlock_irq(&current->sighand->siglock);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 11, 0)
 	// https://github.com/torvalds/linux/commit/bfafe5efa9754ebc991750da0bcca2a6694f3ed3#diff-45eb79a57536d8eccfc1436932f093eb5c0b60d9361c39edb46581ad313e8987R576-R577
 	fake->flags |= PF_EXITING;
@@ -152,6 +152,7 @@ void disable_seccomp(void)
 void escape_with_root_profile(void)
 {
 	struct cred *cred;
+	struct root_profile profile;
 #ifdef CONFIG_KSU_SYSCALL_HOOK
 	struct task_struct *t;
 #endif
@@ -167,35 +168,34 @@ void escape_with_root_profile(void)
 		return;
 	}
 
-	struct root_profile *profile = ksu_get_root_profile(cred->uid.val);
+	ksu_get_root_profile(cred->uid.val, &profile);
 
-	cred->uid.val = profile->uid;
-	cred->suid.val = profile->uid;
-	cred->euid.val = profile->uid;
-	cred->fsuid.val = profile->uid;
+	cred->uid.val = profile.uid;
+	cred->suid.val = profile.uid;
+	cred->euid.val = profile.uid;
+	cred->fsuid.val = profile.uid;
 
-	cred->gid.val = profile->gid;
-	cred->fsgid.val = profile->gid;
-	cred->sgid.val = profile->gid;
-	cred->egid.val = profile->gid;
+	cred->gid.val = profile.gid;
+	cred->fsgid.val = profile.gid;
+	cred->sgid.val = profile.gid;
+	cred->egid.val = profile.gid;
 	cred->securebits = 0;
 
-	BUILD_BUG_ON(sizeof(profile->capabilities.effective) !=
-		     sizeof(kernel_cap_t));
+	BUILD_BUG_ON(sizeof(profile.capabilities.effective) !=
+				 sizeof(kernel_cap_t));
 
 	// setup capabilities
 	// we need CAP_DAC_READ_SEARCH becuase `/data/adb/ksud` is not accessible for non root process
 	// we add it here but don't add it to cap_inhertiable, it would be dropped automaticly after exec!
-	u64 cap_for_ksud =
-		profile->capabilities.effective | CAP_DAC_READ_SEARCH;
-	memcpy(&cred->cap_effective, &cap_for_ksud,
-	       sizeof(cred->cap_effective));
-	memcpy(&cred->cap_permitted, &profile->capabilities.effective,
-	       sizeof(cred->cap_permitted));
-	memcpy(&cred->cap_bset, &profile->capabilities.effective,
-	       sizeof(cred->cap_bset));
+	u64 cap_for_ksud = profile.capabilities.effective | CAP_DAC_READ_SEARCH;
+	memcpy(&cred->cap_effective, &cap_for_ksud, sizeof(cred->cap_effective));
+	memcpy(&cred->cap_permitted, &profile.capabilities.effective,
+		   sizeof(cred->cap_permitted));
+	memcpy(&cred->cap_bset, &profile.capabilities.effective,
+		   sizeof(cred->cap_bset));
 
-	setup_groups(profile, cred);
+	setup_groups(&profile, cred);
+	setup_selinux(profile.selinux_domain, cred);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0)
 	setup_selinux(profile->selinux_domain, current->cred);
@@ -213,7 +213,7 @@ void escape_with_root_profile(void)
 	}
 #endif
 
-	setup_mount_ns(profile->namespaces);
+	setup_mount_ns(profile.namespaces);
 }
 
 void escape_to_root_for_init(void)
@@ -239,7 +239,7 @@ void escape_to_root_for_init(void)
 static void disable_seccomp_for_task(struct task_struct *tsk)
 {
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	struct task_struct *fake;
 	fake = kmalloc(sizeof(*fake), GFP_ATOMIC);
 	if (!fake) {
@@ -255,24 +255,24 @@ static void disable_seccomp_for_task(struct task_struct *tsk)
 	clear_tsk_thread_flag(tsk, TIF_SECCOMP);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	memcpy(fake, tsk, sizeof(*fake));
 #endif
 	tsk->seccomp.mode = 0;
 #if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 9, 0) &&                           \
-     !defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 !defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 	// put_seccomp_filter is allowed while we holding sighand
 	put_seccomp_filter(tsk);
 #endif
 	tsk->seccomp.filter = NULL;
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 7, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_CNT))
 	atomic_set(&tsk->seccomp.filter_count, 0);
 #endif
 	spin_unlock_irq(&tsk->sighand->siglock);
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) ||                          \
-     defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
+	 defined(KSU_OPTIONAL_SECCOMP_FILTER_RELEASE))
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 11, 0)
 	// https://github.com/torvalds/linux/commit/bfafe5efa9754ebc991750da0bcca2a6694f3ed3#diff-45eb79a57536d8eccfc1436932f093eb5c0b60d9361c39edb46581ad313e8987R576-R577
 	fake->flags |= PF_EXITING;
@@ -318,6 +318,7 @@ void escape_to_root_for_cmd_su(uid_t target_uid, pid_t target_pid)
 	struct cred *newcreds;
 	struct task_struct *target_task;
 	unsigned long flags;
+	struct root_profile profile;
 #ifdef CONFIG_KSU_SYSCALL_HOOK
 	struct task_struct *t;
 #endif // #ifndef CONFIG_KSU_SUSFS
@@ -331,7 +332,7 @@ void escape_to_root_for_cmd_su(uid_t target_uid, pid_t target_pid)
 	if (!target_task) {
 		rcu_read_unlock();
 		pr_err("cmd_su: target task not found for PID: %d\n",
-		       target_pid);
+			   target_pid);
 		return;
 	}
 	get_task_struct(target_task);
@@ -347,35 +348,35 @@ void escape_to_root_for_cmd_su(uid_t target_uid, pid_t target_pid)
 	newcreds = prepare_kernel_cred(target_task);
 	if (newcreds == NULL) {
 		pr_err("cmd_su: failed to allocate new cred for PID: %d\n",
-		       target_pid);
+			   target_pid);
 		put_task_struct(target_task);
 		return;
 	}
 
-	struct root_profile *profile = ksu_get_root_profile(target_uid);
+	ksu_get_root_profile(newcreds->uid.val, &profile);
 
-	newcreds->uid.val = profile->uid;
-	newcreds->suid.val = profile->uid;
-	newcreds->euid.val = profile->uid;
-	newcreds->fsuid.val = profile->uid;
+	newcreds->uid.val = profile.uid;
+	newcreds->suid.val = profile.uid;
+	newcreds->euid.val = profile.uid;
+	newcreds->fsuid.val = profile.uid;
 
-	newcreds->gid.val = profile->gid;
-	newcreds->fsgid.val = profile->gid;
-	newcreds->sgid.val = profile->gid;
-	newcreds->egid.val = profile->gid;
+	newcreds->gid.val = profile.gid;
+	newcreds->fsgid.val = profile.gid;
+	newcreds->sgid.val = profile.gid;
+	newcreds->egid.val = profile.gid;
 	newcreds->securebits = 0;
 
-	u64 cap_for_cmd_su = profile->capabilities.effective |
-			     CAP_DAC_READ_SEARCH | CAP_SETUID | CAP_SETGID;
-	memcpy(&newcreds->cap_effective, &cap_for_cmd_su,
-	       sizeof(newcreds->cap_effective));
-	memcpy(&newcreds->cap_permitted, &profile->capabilities.effective,
-	       sizeof(newcreds->cap_permitted));
-	memcpy(&newcreds->cap_bset, &profile->capabilities.effective,
-	       sizeof(newcreds->cap_bset));
+	u64 cap_for_cmd_su = profile.capabilities.effective | CAP_DAC_READ_SEARCH |
+						 CAP_SETUID | CAP_SETGID;
+	memcpy(&newcreds.cap_effective, &cap_for_cmd_su,
+		   sizeof(newcreds.cap_effective));
+	memcpy(&newcreds->cap_permitted, &profile.capabilities.effective,
+		   sizeof(newcreds.cap_permitted));
+	memcpy(&newcreds.cap_bset, &profile.capabilities.effective,
+		   sizeof(newcreds.cap_bset));
 
-	setup_groups(profile, newcreds);
-	setup_selinux(profile->selinux_domain, newcreds);
+	setup_groups(&profile, cred);
+	setup_selinux(profile.selinux_domain, newcreds);
 	task_lock(target_task);
 
 	const struct cred *old_creds = get_task_cred(target_task);
@@ -404,7 +405,7 @@ void escape_to_root_for_cmd_su(uid_t target_uid, pid_t target_pid)
 		ksu_set_task_tracepoint_flag(t);
 	}
 #endif // #ifndef CONFIG_KSU_SUSFS
-	setup_mount_ns(profile->namespaces);
+	setup_mount_ns(profile.namespaces);
 	pr_info("cmd_su: privilege escalation completed for UID: %d, PID: %d\n",
 		target_uid, target_pid);
 }

kernel/allowlist.h
@@ -28,16 +28,19 @@ bool __ksu_is_allow_uid_for_current(uid_t uid);
 #define ksu_is_allow_uid_for_current(uid)                                      \
 	unlikely(__ksu_is_allow_uid_for_current(uid))
 
-bool ksu_get_allow_list(int *array, int *length, bool allow);
+bool ksu_get_allow_list(int *array, u16 length, u16 *out_length, u16 *out_total,
+                        bool allow);
 
 void ksu_prune_allowlist(bool (*is_uid_exist)(uid_t, char *, void *),
 			 void *data);
 
+void ksu_persistent_allow_list();
+
 bool ksu_get_app_profile(struct app_profile *);
-bool ksu_set_app_profile(struct app_profile *, bool persist);
+int ksu_set_app_profile(struct app_profile *);
 
 bool ksu_uid_should_umount(uid_t uid);
-struct root_profile *ksu_get_root_profile(uid_t uid);
+void ksu_get_root_profile(uid_t uid, struct root_profile *);
 
 static inline bool is_appuid(uid_t uid)
 {

kernel/manager.h
@@ -20,6 +20,11 @@ static inline bool is_manager()
 			current_uid().val % PER_USER_RANGE);
 }
 
+static inline bool is_uid_manager(uid_t uid)
+{
+    return unlikely(ksu_manager_appid == uid % PER_USER_RANGE);
+}
+
 static inline uid_t ksu_get_manager_appid()
 {
 	return ksu_manager_appid;

kernel/supercalls.c
@@ -178,50 +178,111 @@ static int do_check_safemode(void __user *arg)
 	return 0;
 }
 
-static int do_get_allow_list(void __user *arg)
+static int do_new_get_allow_list_common(void __user *arg, bool allow)
 {
-	struct ksu_get_allow_list_cmd cmd;
+	struct ksu_new_get_allow_list_cmd cmd;
+	int *arr = NULL;
+	int err = 0;
 
 	if (copy_from_user(&cmd, arg, sizeof(cmd))) {
 		return -EFAULT;
 	}
 
+	if (cmd.count) {
+		arr = kmalloc(sizeof(int) * cmd.count, GFP_KERNEL);
+		if (!arr) {
+			return -ENOMEM;
+		}
+	}
+
 	bool success =
-		ksu_get_allow_list((int *)cmd.uids, (int *)&cmd.count, true);
+		ksu_get_allow_list(arr, cmd.count, &cmd.count, &cmd.total_count, allow);
 
 	if (!success) {
-		return -EFAULT;
+		err = -EFAULT;
+		goto out;
 	}
 
 	if (copy_to_user(arg, &cmd, sizeof(cmd))) {
-		pr_err("get_allow_list: copy_to_user failed\n");
-		return -EFAULT;
+		pr_err("new_get_allow_list: copy_to_user count failed\n");
+		err = -EFAULT;
+		goto out;
 	}
 
-	return 0;
+	if (cmd.count &&
+		copy_to_user(&((struct ksu_new_get_allow_list_cmd *)arg)->uids, arr,
+					 sizeof(int) * cmd.count)) {
+		pr_err("new_get_allow_list: copy_to_user uids failed\n");
+		err = -EFAULT;
+	}
+
+out:
+	if (arr) {
+		kfree(arr);
+	}
+	return err;
+}
+
+static int do_new_get_deny_list(void __user *arg)
+{
+	return do_new_get_allow_list_common(arg, false);
+}
+
+static int do_new_get_allow_list(void __user *arg)
+{
+	return do_new_get_allow_list_common(arg, true);
+}
+
+static int do_get_allow_list_common(void __user *arg, bool allow)
+{
+	int *arr = NULL;
+	int err = 0;
+	u16 count;
+	u32 out_count;
+	static const u16 kSize = 128;
+
+	arr = kmalloc(sizeof(int) * kSize, GFP_KERNEL);
+	if (!arr) {
+		return -ENOMEM;
+	}
+
+	bool success = ksu_get_allow_list(arr, kSize, &count, NULL, allow);
+
+
+	if (!success) {
+		err = -EFAULT;
+		goto out;
+	}
+
+	out_count = count;
+
+	if (copy_to_user(arg + offsetof(struct ksu_get_allow_list_cmd, count),
+					 &out_count, sizeof(u32))) {
+		pr_err("get_allow_list: copy_to_user count failed\n");
+		err = -EFAULT;
+		goto out;
+	}
+
+	if (copy_to_user(arg, arr, sizeof(u32) * count)) {
+		pr_err("get_allow_list: copy_to_user uids failed\n");
+		err = -EFAULT;
+	}
+
+out:
+	if (arr) {
+		kfree(arr);
+	}
+	return err;
 }
 
 static int do_get_deny_list(void __user *arg)
 {
-	struct ksu_get_allow_list_cmd cmd;
+	return do_get_allow_list_common(arg, false);
+}
 
-	if (copy_from_user(&cmd, arg, sizeof(cmd))) {
-		return -EFAULT;
-	}
-
-	bool success =
-		ksu_get_allow_list((int *)cmd.uids, (int *)&cmd.count, false);
-
-	if (!success) {
-		return -EFAULT;
-	}
-
-	if (copy_to_user(arg, &cmd, sizeof(cmd))) {
-		pr_err("get_deny_list: copy_to_user failed\n");
-		return -EFAULT;
-	}
-
-	return 0;
+static int do_get_allow_list(void __user *arg)
+{
+	return do_get_allow_list_common(arg, true);
 }
 
 static int do_uid_granted_root(void __user *arg)
@@ -298,17 +359,20 @@ static int do_get_app_profile(void __user *arg)
 static int do_set_app_profile(void __user *arg)
 {
 	struct ksu_set_app_profile_cmd cmd;
+	int ret;
 
 	if (copy_from_user(&cmd, arg, sizeof(cmd))) {
 		pr_err("set_app_profile: copy_from_user failed\n");
 		return -EFAULT;
 	}
 
-	if (!ksu_set_app_profile(&cmd.profile, true)) {
-		return -EFAULT;
+	ret = ksu_set_app_profile(&cmd.profile);
+	if (!ret) {
+		ksu_persistent_allow_list();
+		ksu_mark_running_process();
 	}
 
-	return 0;
+	return ret;
 }
 
 static int do_get_feature(void __user *arg)
@@ -327,7 +391,7 @@ static int do_get_feature(void __user *arg)
 
 	if (ret && supported) {
 		pr_err("get_feature: failed for feature %u: %d\n",
-		       cmd.feature_id, ret);
+			   cmd.feature_id, ret);
 		return ret;
 	}
 
@@ -352,7 +416,7 @@ static int do_set_feature(void __user *arg)
 	ret = ksu_set_feature(cmd.feature_id, cmd.value);
 	if (ret) {
 		pr_err("set_feature: failed for feature %u: %d\n",
-		       cmd.feature_id, ret);
+			   cmd.feature_id, ret);
 		return ret;
 	}
 
@@ -392,7 +456,7 @@ static int do_manage_mark(void __user *arg)
 		ret = ksu_get_task_mark(cmd.pid);
 		if (ret < 0) {
 			pr_err("manage_mark: get failed for pid %d: %d\n",
-			       cmd.pid, ret);
+				   cmd.pid, ret);
 			return ret;
 		}
 		cmd.result = (u32)ret;
@@ -416,7 +480,7 @@ static int do_manage_mark(void __user *arg)
 			ret = ksu_set_task_mark(cmd.pid, true);
 			if (ret < 0) {
 				pr_err("manage_mark: set_mark failed for pid %d: %d\n",
-				       cmd.pid, ret);
+					   cmd.pid, ret);
 				return ret;
 			}
 		}
@@ -435,7 +499,7 @@ static int do_manage_mark(void __user *arg)
 			ret = ksu_set_task_mark(cmd.pid, false);
 			if (ret < 0) {
 				pr_err("manage_mark: set_unmark failed for pid %d: %d\n",
-				       cmd.pid, ret);
+					   cmd.pid, ret);
 				return ret;
 			}
 		}
@@ -502,7 +566,7 @@ static int add_try_umount(void __user *arg)
 
 	case KSU_UMOUNT_ADD: {
 		long len = strncpy_from_user(buf, (const char __user *)cmd.arg,
-					     256);
+						 256);
 		if (len <= 0)
 			return -EFAULT;
 
@@ -551,7 +615,7 @@ static int add_try_umount(void __user *arg)
 	// this is just strcmp'd wipe anyway
 	case KSU_UMOUNT_DEL: {
 		long len = strncpy_from_user(buf, (const char __user *)cmd.arg,
-					     sizeof(buf) - 1);
+						 sizeof(buf) - 1);
 		if (len <= 0)
 			return -EFAULT;
 
@@ -659,21 +723,21 @@ static int list_try_umount(void __user *arg)
 		output_size = MAX_UMOUNT_LIST_SIZE;
 
 	pr_info("KernelSU: Allocating %zu bytes for %d mounts (user requested %zu)\n",
-	        output_size, mount_count, (size_t)cmd.buf_size);
+			output_size, mount_count, (size_t)cmd.buf_size);
 
 	// Try kzalloc first with NOWARN flag
 	output_buf = kzalloc(output_size, GFP_KERNEL | __GFP_NOWARN);
 	if (!output_buf) {
 		// Fallback to vzalloc for large allocations
 		pr_info("KernelSU: kzalloc failed for %zu bytes, using vzalloc\n", 
-		        output_size);
+				output_size);
 		output_buf = vzalloc(output_size);
 		using_vmalloc = true;
 	}
 
 	if (!output_buf) {
 		pr_err("KernelSU: Failed to allocate %zu bytes for umount list\n",
-		       output_size);
+			   output_size);
 		return -ENOMEM;
 	}
 	offset += snprintf(output_buf + offset, output_size - offset,
@@ -812,16 +876,16 @@ static int do_manual_su(void __user *arg)
 	request.target_pid = cmd.target_pid;
 
 	if (cmd.option == MANUAL_SU_OP_GENERATE_TOKEN ||
-	    cmd.option == MANUAL_SU_OP_ESCALATE) {
+		cmd.option == MANUAL_SU_OP_ESCALATE) {
 		memcpy(request.token_buffer, cmd.token_buffer,
-		       sizeof(request.token_buffer));
+			   sizeof(request.token_buffer));
 	}
 
 	res = ksu_handle_manual_su_request(cmd.option, &request);
 
 	if (cmd.option == MANUAL_SU_OP_GENERATE_TOKEN && res == 0) {
 		memcpy(cmd.token_buffer, request.token_buffer,
-		       sizeof(cmd.token_buffer));
+			   sizeof(cmd.token_buffer));
 		if (copy_to_user(arg, &cmd, sizeof(cmd))) {
 			pr_err("manual_su: copy_to_user failed\n");
 			return -EFAULT;
@@ -843,6 +907,10 @@ static const struct ksu_ioctl_cmd_map ksu_ioctl_handlers[] = {
 	KSU_IOCTL(GET_ALLOW_LIST, "GET_ALLOW_LIST", do_get_allow_list,
 		  manager_or_root),
 	KSU_IOCTL(GET_DENY_LIST, "GET_DENY_LIST", do_get_deny_list,
+		  manager_or_root),
+	KSU_IOCTL(KSU_IOCTL_NEW_GET_ALLOW_LIST, "NEW_GET_ALLOW_LIST", do_new_get_allow_list,
+		  manager_or_root),
+	KSU_IOCTL(KSU_IOCTL_NEW_GET_DENY_LIST, "NEW_GET_DENY_LIST", do_new_get_deny_list,
 		  manager_or_root),
 	KSU_IOCTL(UID_GRANTED_ROOT, "UID_GRANTED_ROOT", do_uid_granted_root,
 		  manager_or_root),
@@ -1117,7 +1185,7 @@ static long anon_ksu_ioctl(struct file *filp, unsigned int cmd,
 		if (cmd == ksu_ioctl_handlers[i].cmd) {
 			// Check permission first
 			if (ksu_ioctl_handlers[i].perm_check &&
-			    !ksu_ioctl_handlers[i].perm_check()) {
+				!ksu_ioctl_handlers[i].perm_check()) {
 				pr_warn("ksu ioctl: permission denied for cmd=0x%x uid=%d\n",
 					cmd, current_uid().val);
 				return -EPERM;

kernel/supercalls.h
@@ -39,10 +39,17 @@ struct ksu_check_safemode_cmd {
 	__u8 in_safe_mode; // Output: true if in safe mode, false otherwise
 };
 
+// deprecated
 struct ksu_get_allow_list_cmd {
 	__u32 uids[128]; // Output: array of allowed/denied UIDs
 	__u32 count; // Output: number of UIDs in array
 	__u8 allow; // Input: true for allow list, false for deny list
+};
+
+struct ksu_new_get_allow_list_cmd {
+    __u16 count; // Input / Output: number of UIDs in array
+    __u16 total_count; // Output: total number of UIDs in requested list
+    __u32 uids[0]; // Output: array of allowed/denied UIDs
 };
 
 struct ksu_uid_granted_root_cmd {
@@ -142,8 +149,14 @@ struct ksu_manual_su_cmd {
 #define KSU_IOCTL_REPORT_EVENT _IOC(_IOC_WRITE, 'K', 3, 0)
 #define KSU_IOCTL_SET_SEPOLICY _IOC(_IOC_READ | _IOC_WRITE, 'K', 4, 0)
 #define KSU_IOCTL_CHECK_SAFEMODE _IOC(_IOC_READ, 'K', 5, 0)
+// deprecated
 #define KSU_IOCTL_GET_ALLOW_LIST _IOC(_IOC_READ | _IOC_WRITE, 'K', 6, 0)
+// deprecated
 #define KSU_IOCTL_GET_DENY_LIST _IOC(_IOC_READ | _IOC_WRITE, 'K', 7, 0)
+#define KSU_IOCTL_NEW_GET_ALLOW_LIST                                           \
+    _IOWR('K', 6, struct ksu_new_get_allow_list_cmd)
+#define KSU_IOCTL_NEW_GET_DENY_LIST                                            \
+    _IOWR('K', 7, struct ksu_new_get_allow_list_cmd)
 #define KSU_IOCTL_UID_GRANTED_ROOT _IOC(_IOC_READ | _IOC_WRITE, 'K', 8, 0)
 #define KSU_IOCTL_UID_SHOULD_UMOUNT _IOC(_IOC_READ | _IOC_WRITE, 'K', 9, 0)
 #define KSU_IOCTL_GET_MANAGER_APPID _IOC(_IOC_READ, 'K', 10, 0)

commit 8560c5fd2518296defdb12ab761431513cf093ab

fix: app_profile.c syntax errors from recent commits
- Fix incorrect pointer/structure access in setup_selinux call
- Fix 'newcreds' pointer usage (should be -> not .)
- Add missing 'cred' variable or use 'newcreds' in setup_groups call

These errors were introduced in d00c156 commit.

Fixes build failure in builtin branch when compiling with SUSFS enabled.

Signed-off-by: Ahmed Al-Nassif <mr.ahmed.nassif@gmail.com>

kernel/app_profile.c
@@ -200,7 +200,7 @@ void escape_with_root_profile(void)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0) && LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0)
 	setup_selinux(profile->selinux_domain, current->cred);
 #else
-	setup_selinux(profile->selinux_domain, cred);
+	setup_selinux(profile.selinux_domain, cred);
 #endif
 
 	commit_creds(cred);
@@ -368,14 +368,14 @@ void escape_to_root_for_cmd_su(uid_t target_uid, pid_t target_pid)
 
 	u64 cap_for_cmd_su = profile.capabilities.effective | CAP_DAC_READ_SEARCH |
 						 CAP_SETUID | CAP_SETGID;
-	memcpy(&newcreds.cap_effective, &cap_for_cmd_su,
-		   sizeof(newcreds.cap_effective));
+	memcpy(&newcreds->cap_effective, &cap_for_cmd_su,
+		sizeof(newcreds->cap_effective));
 	memcpy(&newcreds->cap_permitted, &profile.capabilities.effective,
-		   sizeof(newcreds.cap_permitted));
-	memcpy(&newcreds.cap_bset, &profile.capabilities.effective,
-		   sizeof(newcreds.cap_bset));
+		sizeof(newcreds->cap_permitted));
+	memcpy(&newcreds->cap_bset, &profile.capabilities.effective,
+		sizeof(newcreds->cap_bset));
 
-	setup_groups(&profile, cred);
+	setup_groups(&profile, newcreds);
 	setup_selinux(profile.selinux_domain, newcreds);
 	task_lock(target_task);
 