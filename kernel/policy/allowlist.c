e5506ed65eac6491ac20c7a91bb40436cfafcc7e
Ylarod <me@ylarod.cn>
2026-03-26 22:19:09 +0800

    kernel: allow disable manager or policy

--- kernel/policy/allowlist.c
+++ kernel/policy/allowlist.c
@@ -141,6 +141,11 @@ static bool profile_valid(struct app_profile *profile)
     }
 
     if (profile->allow_su) {
+#ifndef CONFIG_KSU_DISABLE_POLICY
+        if (profile->rp_config.use_default) {
+            return true;
+        }
+
         if (profile->rp_config.profile.groups_count > KSU_MAX_GROUPS) {
             return false;
         }
@@ -148,6 +153,7 @@ static bool profile_valid(struct app_profile *profile)
         if (strlen(profile->rp_config.profile.selinux_domain) == 0) {
             return false;
         }
+#endif
     }
 
     return true;
@@ -163,6 +169,17 @@ int ksu_set_app_profile(struct app_profile *profile)
         pr_err("Failed to set app profile: invalid profile!\n");
         return -EINVAL;
     }
+
+#ifdef CONFIG_KSU_DISABLE_POLICY
+    if (profile->allow_su) {
+        profile->rp_config.use_default = true;
+        memset(profile->rp_config.template_name, 0, sizeof(profile->rp_config.template_name));
+        memset(&profile->rp_config.profile, 0, sizeof(profile->rp_config.profile));
+    } else {
+        profile->nrp_config.use_default = true;
+        memset(&profile->nrp_config.profile, 0, sizeof(profile->nrp_config.profile));
+    }
+#endif
 
     mutex_lock(&allowlist_mutex);
 
@@ -213,12 +230,16 @@ out:
 
     // check if the default profiles is changed, cache it to a single struct to accelerate access.
     if (unlikely(!strcmp(profile->key, "$"))) {
+#ifndef CONFIG_KSU_DISABLE_POLICY
         // set default non root profile
         memcpy(&default_non_root_profile, &profile->nrp_config.profile, sizeof(default_non_root_profile));
+#endif
     } else if (unlikely(!strcmp(profile->key, "#"))) {
+#ifndef CONFIG_KSU_DISABLE_POLICY
         // set default root profile
         // TODO: Do we really need this?
         memcpy(&default_root_profile, &profile->rp_config.profile, sizeof(default_root_profile));
+#endif
     } else if (profile->current_uid <= BITMAP_UID_MAX) {
         if (profile->allow_su)
             allow_list_bitmap[profile->current_uid / BITS_PER_BYTE] |= 1 << (profile->current_uid % BITS_PER_BYTE);
@@ -255,7 +276,7 @@ bool __ksu_is_allow_uid(uid_t uid)
         return false;
     }
 
-    if (likely(ksu_is_manager_appid_valid()) && unlikely(ksu_get_manager_appid() == uid % PER_USER_RANGE)) {
+    if (unlikely(is_uid_manager(uid))) {
         // manager is always allowed!
         return true;
     }
@@ -287,11 +308,16 @@ bool __ksu_is_allow_uid_for_current(uid_t uid)
 
 bool ksu_uid_should_umount(uid_t uid)
 {
+#ifndef CONFIG_KSU_DISABLE_POLICY
     struct app_profile profile = { .current_uid = uid };
-    if (likely(ksu_is_manager_appid_valid()) && unlikely(ksu_get_manager_appid() == uid % PER_USER_RANGE)) {
+#endif
+    if (unlikely(is_uid_manager(uid))) {
         // we should not umount on manager!
         return false;
     }
+#ifdef CONFIG_KSU_DISABLE_POLICY
+    return !__ksu_is_allow_uid(uid);
+#else
     bool found = ksu_get_app_profile(&profile);
     if (!found) {
         // no app profile found, it must be non root app
@@ -308,11 +334,20 @@ bool ksu_uid_should_umount(uid_t uid)
             return profile.nrp_config.profile.umount_modules;
         }
     }
+#endif
 }
 
 void ksu_get_root_profile(uid_t uid, struct root_profile *profile)
 {
+#ifndef CONFIG_KSU_DISABLE_POLICY
     struct perm_data *p = NULL;
+#endif
+
+#ifdef CONFIG_KSU_DISABLE_POLICY
+    (void)uid;
+    memcpy(profile, &default_root_profile, sizeof(*profile));
+    return;
+#endif
 
     if (is_uid_manager(uid)) {
         goto use_default;
@@ -433,6 +468,11 @@ put_task:
 
 void ksu_load_allow_list()
 {
+#ifdef CONFIG_KSU_DISABLE_POLICY
+    pr_info("allowlist load skipped because policy is disabled\n");
+    return;
+#endif
+
     loff_t off = 0;
     ssize_t ret = 0;
     struct file *fp = NULL;
