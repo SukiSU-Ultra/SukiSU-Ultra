tiann/KernelSU

manager: Only trigger side effects after page is settled

manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -96,6 +96,7 @@ import me.weishu.kernelsu.ui.theme.LocalEnableFloatingBottomBarBlur
 import me.weishu.kernelsu.ui.util.LocalSnackbarHost
 import me.weishu.kernelsu.ui.util.getFileName
 import me.weishu.kernelsu.ui.util.install
+import me.weishu.kernelsu.ui.util.rememberContentReady
 import me.weishu.kernelsu.ui.util.rootAvailable
 import me.weishu.kernelsu.ui.viewmodel.MainActivityViewModel
 import me.weishu.kernelsu.ui.webui.WebUIActivity
@@ -260,7 +261,7 @@ fun MainScreen() {
     CompositionLocalProvider(
         LocalMainPagerState provides mainPagerState
     ) {
-        val contentReady = me.weishu.kernelsu.ui.util.rememberContentReady()
+        val contentReady = rememberContentReady()
         val pagerContent = @Composable { bottomInnerPadding: Dp ->
             HorizontalPager(
                 modifier = Modifier
@@ -270,11 +271,11 @@ fun MainScreen() {
                 beyondViewportPageCount = if (contentReady) 3 else 0,
                 userScrollEnabled = userScrollEnabled,
             ) { page ->
-                val isCurrentPage = page == mainPagerState.pagerState.currentPage
+                val isCurrentPage = page == mainPagerState.pagerState.settledPage
                 when (page) {
-                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding)
-                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding)
-                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding)
+                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding, isCurrentPage)
+                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding, isCurrentPage)
+                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding, isCurrentPage)
                     3 -> if (isCurrentPage || contentReady) SettingPager(navController, bottomInnerPadding)
                 }
             }

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -103,6 +103,7 @@ import me.weishu.kernelsu.ui.component.material.SegmentedListItem
 import me.weishu.kernelsu.ui.component.material.TonalCard
 import me.weishu.kernelsu.ui.component.statustag.StatusTag
 import me.weishu.kernelsu.ui.util.download
+import me.weishu.kernelsu.ui.util.rememberContentReady
 import java.text.Collator
 
 @SuppressLint("LocalContextGetResourceValueCall")
@@ -184,7 +185,7 @@ fun ModuleRepoScreenMaterial(
     ) { innerPadding ->
         val isLoading = state.modules.isEmpty()
         val hadDataOnEntry = remember { state.modules.isNotEmpty() }
-        val contentReady = hadDataOnEntry || me.weishu.kernelsu.ui.util.rememberContentReady()
+        val contentReady = hadDataOnEntry || rememberContentReady()
 
         if (!contentReady || isLoading) {
             Box(
@@ -480,7 +481,7 @@ private fun ReadmePage(
         ),
     ) {
         item {
-            val contentReady = me.weishu.kernelsu.ui.util.rememberContentReady()
+            val contentReady = rememberContentReady()
             var isLoading by remember { mutableStateOf(true) }
             if (isLoading) {
                 Box(

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -84,6 +84,7 @@ import me.weishu.kernelsu.ui.theme.LocalEnableBlur
 import me.weishu.kernelsu.ui.theme.isInDarkTheme
 import me.weishu.kernelsu.ui.util.defaultHazeEffect
 import me.weishu.kernelsu.ui.util.download
+import me.weishu.kernelsu.ui.util.rememberContentReady
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CircularProgressIndicator
 import top.yukonga.miuix.kmp.basic.DropdownImpl
@@ -318,7 +319,7 @@ fun ModuleRepoScreenMiuix(
         val layoutDirection = LocalLayoutDirection.current
         val isLoading = state.modules.isEmpty()
         val hadDataOnEntry = remember { state.modules.isNotEmpty() }
-        val contentReady = hadDataOnEntry || me.weishu.kernelsu.ui.util.rememberContentReady()
+        val contentReady = hadDataOnEntry || rememberContentReady()
         val offline = state.offline
 
         searchStatus.SearchBox(
@@ -345,7 +346,11 @@ fun ModuleRepoScreenMiuix(
                 ) {
                     if (offline) {
                         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = stringResource(R.string.network_offline), color = colorScheme.onSurfaceVariantSummary, fontSize = 16.sp)
+                            Text(
+                                text = stringResource(R.string.network_offline),
+                                color = colorScheme.onSurfaceVariantSummary,
+                                fontSize = 16.sp
+                            )
                             Spacer(Modifier.height(12.dp))
                             TextButton(
                                 modifier = Modifier
@@ -538,7 +543,7 @@ private fun ReadmePage(
         overscrollEffect = null,
     ) {
         item {
-            val contentReady = me.weishu.kernelsu.ui.util.rememberContentReady()
+            val contentReady = rememberContentReady()
             var isLoading by remember { mutableStateOf(true) }
             if (isLoading) {
                 Box(

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/MainActivity.kt
@@ -61,11 +61,12 @@ import dev.chrisbanes.haze.HazeTint
 import dev.chrisbanes.haze.hazeSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import com.sukisu.ultra.Natives
+import com.sukisu.ultra.R
 import com.sukisu.ultra.ui.component.bottombar.BottomBar
 import com.sukisu.ultra.ui.component.bottombar.MainPagerState
 import com.sukisu.ultra.ui.component.bottombar.SideRail
 import com.sukisu.ultra.ui.component.bottombar.rememberMainPagerState
-import com.sukisu.ultra.ui.kernelFlash.KernelFlashScreen
+import com.sukisu.ultra.ui.component.dialog.rememberConfirmDialog
 import com.sukisu.ultra.ui.navigation3.HandleDeepLink
 import com.sukisu.ultra.ui.navigation3.LocalNavigator
 import com.sukisu.ultra.ui.navigation3.Navigator
@@ -75,28 +76,26 @@ import com.sukisu.ultra.ui.screen.about.AboutScreen
 import com.sukisu.ultra.ui.screen.appprofile.AppProfileScreen
 import com.sukisu.ultra.ui.screen.colorpalette.ColorPaletteScreen
 import com.sukisu.ultra.ui.screen.executemoduleaction.ExecuteModuleActionScreen
+import com.sukisu.ultra.ui.screen.flash.FlashIt
 import com.sukisu.ultra.ui.screen.flash.FlashScreen
 import com.sukisu.ultra.ui.screen.home.HomePager
 import com.sukisu.ultra.ui.screen.install.InstallScreen
-import com.sukisu.ultra.ui.screen.kpm.KpmScreen
 import com.sukisu.ultra.ui.screen.module.ModulePager
 import com.sukisu.ultra.ui.screen.modulerepo.ModuleRepoDetailScreen
 import com.sukisu.ultra.ui.screen.modulerepo.ModuleRepoScreen
 import com.sukisu.ultra.ui.screen.settings.SettingPager
-import com.sukisu.ultra.ui.screen.settings.tools.ToolsScreen
-import com.sukisu.ultra.ui.screen.sulog.SulogScreen
 import com.sukisu.ultra.ui.screen.superuser.SuperUserPager
 import com.sukisu.ultra.ui.screen.template.AppProfileTemplateScreen
 import com.sukisu.ultra.ui.screen.templateeditor.TemplateEditorScreen
-import com.sukisu.ultra.ui.screen.umountmanager.UmountManagerScreen
-import com.sukisu.ultra.ui.screen.susfs.SuSFSScreen
 import com.sukisu.ultra.ui.theme.KernelSUTheme
 import com.sukisu.ultra.ui.theme.LocalColorMode
 import com.sukisu.ultra.ui.theme.LocalEnableBlur
 import com.sukisu.ultra.ui.theme.LocalEnableFloatingBottomBar
 import com.sukisu.ultra.ui.theme.LocalEnableFloatingBottomBarBlur
 import com.sukisu.ultra.ui.util.LocalSnackbarHost
+import com.sukisu.ultra.ui.util.getFileName
 import com.sukisu.ultra.ui.util.install
+import com.sukisu.ultra.ui.util.rememberContentReady
 import com.sukisu.ultra.ui.util.rootAvailable
 import com.sukisu.ultra.ui.viewmodel.MainActivityViewModel
 import com.sukisu.ultra.ui.webui.WebUIActivity
@@ -285,7 +284,7 @@ fun MainScreen() {
     CompositionLocalProvider(
         LocalMainPagerState provides mainPagerState
     ) {
-        val contentReady = com.sukisu.ultra.ui.util.rememberContentReady()
+        val contentReady = rememberContentReady()
         val pagerContent = @Composable { bottomInnerPadding: Dp ->
             HorizontalPager(
                 modifier = Modifier
@@ -295,11 +294,11 @@ fun MainScreen() {
                 beyondViewportPageCount = if (contentReady) 3 else 0,
                 userScrollEnabled = userScrollEnabled,
             ) { page ->
-                val isCurrentPage = page == mainPagerState.pagerState.currentPage
+                val isCurrentPage = page == mainPagerState.pagerState.settledPage
                 when (page) {
-                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding)
-                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding)
-                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding)
+                    0 -> if (isCurrentPage || contentReady) HomePager(navController, bottomInnerPadding, isCurrentPage)
+                    1 -> if (isCurrentPage || contentReady) SuperUserPager(navController, bottomInnerPadding, isCurrentPage)
+                    2 -> if (isCurrentPage || contentReady) ModulePager(bottomInnerPadding, isCurrentPage)
                     3 -> if (isCurrentPage || contentReady) SettingPager(navController, bottomInnerPadding)
                 }
             }

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -103,6 +103,7 @@ import com.sukisu.ultra.ui.component.material.SegmentedListItem
 import com.sukisu.ultra.ui.component.material.TonalCard
 import com.sukisu.ultra.ui.component.statustag.StatusTag
 import com.sukisu.ultra.ui.util.download
+import com.sukisu.ultra.ui.util.rememberContentReady
 import java.text.Collator
 
 @SuppressLint("LocalContextGetResourceValueCall")
@@ -184,7 +185,7 @@ fun ModuleRepoScreenMaterial(
     ) { innerPadding ->
         val isLoading = state.modules.isEmpty()
         val hadDataOnEntry = remember { state.modules.isNotEmpty() }
-        val contentReady = hadDataOnEntry || com.sukisu.ultra.ui.util.rememberContentReady()
+        val contentReady = hadDataOnEntry || rememberContentReady()
 
         if (!contentReady || isLoading) {
             Box(
@@ -480,7 +481,7 @@ private fun ReadmePage(
         ),
     ) {
         item {
-            val contentReady = com.sukisu.ultra.ui.util.rememberContentReady()
+            val contentReady = rememberContentReady()
             var isLoading by remember { mutableStateOf(true) }
             if (isLoading) {
                 Box(

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -84,6 +84,7 @@ import com.sukisu.ultra.ui.theme.LocalEnableBlur
 import com.sukisu.ultra.ui.theme.isInDarkTheme
 import com.sukisu.ultra.ui.util.defaultHazeEffect
 import com.sukisu.ultra.ui.util.download
+import com.sukisu.ultra.ui.util.rememberContentReady
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CircularProgressIndicator
 import top.yukonga.miuix.kmp.basic.DropdownImpl
@@ -318,7 +319,7 @@ fun ModuleRepoScreenMiuix(
         val layoutDirection = LocalLayoutDirection.current
         val isLoading = state.modules.isEmpty()
         val hadDataOnEntry = remember { state.modules.isNotEmpty() }
-        val contentReady = hadDataOnEntry || com.sukisu.ultra.ui.util.rememberContentReady()
+        val contentReady = hadDataOnEntry || rememberContentReady()
         val offline = state.offline
 
         searchStatus.SearchBox(
@@ -345,7 +346,11 @@ fun ModuleRepoScreenMiuix(
                 ) {
                     if (offline) {
                         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                            Text(text = stringResource(R.string.network_offline), color = colorScheme.onSurfaceVariantSummary, fontSize = 16.sp)
+                            Text(
+                                text = stringResource(R.string.network_offline),
+                                color = colorScheme.onSurfaceVariantSummary,
+                                fontSize = 16.sp
+                            )
                             Spacer(Modifier.height(12.dp))
                             TextButton(
                                 modifier = Modifier
@@ -538,7 +543,7 @@ private fun ReadmePage(
         overscrollEffect = null,
     ) {
         item {
-            val contentReady = com.sukisu.ultra.ui.util.rememberContentReady()
+            val contentReady = rememberContentReady()
             var isLoading by remember { mutableStateOf(true) }
             if (isLoading) {
                 Box(