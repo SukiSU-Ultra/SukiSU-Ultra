78b8744105c52af2bc680a5fd1428888efe2928d
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2026-04-01 16:29:09 +0800

    kernel: Add the missing files

---
+++ kernel/manager/throne_tracker.h
@@ -0,0 +1,12 @@
+#ifndef __KSU_H_UID_OBSERVER
+#define __KSU_H_UID_OBSERVER
+
+#include <linux/types.h>
+
+void ksu_throne_tracker_init();
+
+void ksu_throne_tracker_exit();
+
+void track_throne(bool prune_only);
+
+#endif
