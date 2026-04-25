tiann/KernelSU

manager: refactor markdown support

manager/app/src/main/java/me/weishu/kernelsu/ui/component/dialog/DialogMaterial.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,8 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import me.weishu.kernelsu.ui.component.GithubMarkdown
-import me.weishu.kernelsu.ui.component.Markdown
+import me.weishu.kernelsu.ui.component.markdown.MarkdownContent
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -59,8 +57,8 @@ fun ConfirmDialogMaterial(
             text = {
                 visuals.content?.let { content ->
                     when {
-                        visuals.isMarkdown -> Markdown(content = content)
-                        visuals.isHtml -> GithubMarkdown(content = content, containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
+                        visuals.isMarkdown -> MarkdownContent(content = content, isMarkdown = true)
+                        visuals.isHtml -> MarkdownContent(content = content, isMarkdown = false)
                         else -> Text(text = content)
                     }
                 }

manager/app/src/main/java/me/weishu/kernelsu/ui/component/dialog/DialogMiuix.kt
@@ -21,12 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import me.weishu.kernelsu.R
-import me.weishu.kernelsu.ui.component.GithubMarkdown
-import me.weishu.kernelsu.ui.component.Markdown
+import me.weishu.kernelsu.ui.component.markdown.MarkdownContent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.basic.TextButton
+import top.yukonga.miuix.kmp.theme.LocalDismissState
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.window.WindowDialog
 
@@ -75,10 +75,11 @@ fun ConfirmDialogMiuix(
         content = {
             Layout(
                 content = {
+                    val dismissState = LocalDismissState.current
                     visuals.content?.let { content ->
                         when {
-                            visuals.isMarkdown -> Markdown(content = content)
-                            visuals.isHtml -> GithubMarkdown(content = content)
+                            visuals.isMarkdown -> MarkdownContent(content = content, isMarkdown = true)
+                            visuals.isHtml -> MarkdownContent(content = content, isMarkdown = false)
                             else -> Text(text = content)
                         }
                     }
@@ -90,7 +91,7 @@ fun ConfirmDialogMiuix(
                             text = visuals.dismiss ?: stringResource(id = android.R.string.cancel),
                             onClick = {
                                 dismiss()
-                                showDialog.value = false
+                                dismissState?.invoke()
                             },
                             modifier = Modifier.weight(1f)
                         )
@@ -99,7 +100,7 @@ fun ConfirmDialogMiuix(
                             text = visuals.confirm ?: stringResource(id = android.R.string.ok),
                             onClick = {
                                 confirm()
-                                showDialog.value = false
+                                dismissState?.invoke()
                             },
                             modifier = Modifier.weight(1f),
                             colors = ButtonDefaults.textButtonColorsPrimary()

manager/app/src/main/java/me/weishu/kernelsu/ui/component/markdown/MarkdownContent.kt
@@ -1,0 +1,55 @@
+package me.weishu.kernelsu.ui.component.markdown
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import me.weishu.kernelsu.ui.LocalUiMode
+import me.weishu.kernelsu.ui.UiMode
+import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
+
+@Composable
+fun MarkdownContent(
+    content: String,
+    isMarkdown: Boolean,
+) {
+    var isLoading by remember { mutableStateOf(true) }
+    val containerColor = when (LocalUiMode.current) {
+        UiMode.Material -> MaterialTheme.colorScheme.surfaceContainerHigh
+        UiMode.Miuix -> null
+    }
+    Box(Modifier.fillMaxWidth()) {
+        Box(Modifier.verticalScroll(rememberScrollState())) {
+            GithubMarkdown(
+                content = content,
+                isMarkdown = isMarkdown,
+                onLoadingChange = { isLoading = it },
+                containerColor = containerColor,
+            )
+        }
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 64.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                when (LocalUiMode.current) {
+                    UiMode.Material -> LoadingIndicator()
+                    UiMode.Miuix -> InfiniteProgressIndicator()
+                }
+            }
+        }
+    }
+}

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -94,7 +94,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.data.model.RepoModule
-import me.weishu.kernelsu.ui.component.GithubMarkdown
+import me.weishu.kernelsu.ui.component.markdown.GithubMarkdown
 import me.weishu.kernelsu.ui.component.dialog.ConfirmDialogHandle
 import me.weishu.kernelsu.ui.component.dialog.rememberConfirmDialog
 import me.weishu.kernelsu.ui.component.material.SearchAppBar

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -75,7 +75,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import me.weishu.kernelsu.R
-import me.weishu.kernelsu.ui.component.GithubMarkdown
+import me.weishu.kernelsu.ui.component.markdown.GithubMarkdown
 import me.weishu.kernelsu.ui.component.ListPopupDefaults
 import me.weishu.kernelsu.ui.component.SearchStatus
 import me.weishu.kernelsu.ui.component.dialog.ConfirmDialogHandle

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/component/dialog/DialogMaterial.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.LoadingIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -18,8 +17,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import com.sukisu.ultra.ui.component.GithubMarkdown
-import com.sukisu.ultra.ui.component.Markdown
+import com.sukisu.ultra.ui.component.markdown.MarkdownContent
 
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
@@ -59,8 +57,8 @@ fun ConfirmDialogMaterial(
             text = {
                 visuals.content?.let { content ->
                     when {
-                        visuals.isMarkdown -> Markdown(content = content)
-                        visuals.isHtml -> GithubMarkdown(content = content, containerColor = MaterialTheme.colorScheme.surfaceContainerHigh)
+                        visuals.isMarkdown -> MarkdownContent(content = content, isMarkdown = true)
+                        visuals.isHtml -> MarkdownContent(content = content, isMarkdown = false)
                         else -> Text(text = content)
                     }
                 }

manager/app/src/main/java/com/sukisu/ultra/ui/component/dialog/DialogMiuix.kt
@@ -21,12 +21,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.sukisu.ultra.R
-import com.sukisu.ultra.ui.component.GithubMarkdown
-import com.sukisu.ultra.ui.component.Markdown
+import com.sukisu.ultra.ui.component.markdown.MarkdownContent
 import top.yukonga.miuix.kmp.basic.ButtonDefaults
 import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
 import top.yukonga.miuix.kmp.basic.Text
 import top.yukonga.miuix.kmp.basic.TextButton
+import top.yukonga.miuix.kmp.theme.LocalDismissState
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 import top.yukonga.miuix.kmp.window.WindowDialog
 
@@ -75,10 +75,11 @@ fun ConfirmDialogMiuix(
         content = {
             Layout(
                 content = {
+                    val dismissState = LocalDismissState.current
                     visuals.content?.let { content ->
                         when {
-                            visuals.isMarkdown -> Markdown(content = content)
-                            visuals.isHtml -> GithubMarkdown(content = content)
+                            visuals.isMarkdown -> MarkdownContent(content = content, isMarkdown = true)
+                            visuals.isHtml -> MarkdownContent(content = content, isMarkdown = false)
                             else -> Text(text = content)
                         }
                     }
@@ -90,7 +91,7 @@ fun ConfirmDialogMiuix(
                             text = visuals.dismiss ?: stringResource(id = android.R.string.cancel),
                             onClick = {
                                 dismiss()
-                                showDialog.value = false
+                                dismissState?.invoke()
                             },
                             modifier = Modifier.weight(1f)
                         )
@@ -99,7 +100,7 @@ fun ConfirmDialogMiuix(
                             text = visuals.confirm ?: stringResource(id = android.R.string.ok),
                             onClick = {
                                 confirm()
-                                showDialog.value = false
+                                dismissState?.invoke()
                             },
                             modifier = Modifier.weight(1f),
                             colors = ButtonDefaults.textButtonColorsPrimary()

manager/app/src/main/java/com/sukisu/ultra/ui/component/markdown/MarkdownContent.kt
@@ -1,0 +1,55 @@
+package com.sukisu.ultra.ui.component.markdown
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.LoadingIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sukisu.ultra.ui.LocalUiMode
+import com.sukisu.ultra.ui.UiMode
+import top.yukonga.miuix.kmp.basic.InfiniteProgressIndicator
+
+@Composable
+fun MarkdownContent(
+    content: String,
+    isMarkdown: Boolean,
+) {
+    var isLoading by remember { mutableStateOf(true) }
+    val containerColor = when (LocalUiMode.current) {
+        UiMode.Material -> MaterialTheme.colorScheme.surfaceContainerHigh
+        UiMode.Miuix -> null
+    }
+    Box(Modifier.fillMaxWidth()) {
+        Box(Modifier.verticalScroll(rememberScrollState())) {
+            GithubMarkdown(
+                content = content,
+                isMarkdown = isMarkdown,
+                onLoadingChange = { isLoading = it },
+                containerColor = containerColor,
+            )
+        }
+        if (isLoading) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(min = 64.dp),
+                contentAlignment = Alignment.Center,
+            ) {
+                when (LocalUiMode.current) {
+                    UiMode.Material -> LoadingIndicator()
+                    UiMode.Miuix -> InfiniteProgressIndicator()
+                }
+            }
+        }
+    }
+}

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -94,7 +94,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.sukisu.ultra.R
 import com.sukisu.ultra.data.model.RepoModule
-import com.sukisu.ultra.ui.component.GithubMarkdown
+import com.sukisu.ultra.ui.component.markdown.GithubMarkdown
 import com.sukisu.ultra.ui.component.dialog.ConfirmDialogHandle
 import com.sukisu.ultra.ui.component.dialog.rememberConfirmDialog
 import com.sukisu.ultra.ui.component.material.SearchAppBar

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -75,7 +75,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import com.sukisu.ultra.R
-import com.sukisu.ultra.ui.component.GithubMarkdown
+import com.sukisu.ultra.ui.component.markdown.GithubMarkdown
 import com.sukisu.ultra.ui.component.ListPopupDefaults
 import com.sukisu.ultra.ui.component.SearchStatus
 import com.sukisu.ultra.ui.component.dialog.ConfirmDialogHandle