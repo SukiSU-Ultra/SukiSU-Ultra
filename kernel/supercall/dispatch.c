b88403d2561b6e00dff84a3c851e630c62f57fd0
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2026-06-15 16:47:07 +0800

    fix build

--- kernel/supercall/dispatch.c
+++ kernel/supercall/dispatch.c
@@ -43,9 +43,6 @@ static int do_get_info_legacy(void __user *arg)
     if (is_manager()) {
         cmd.flags |= KSU_GET_INFO_FLAG_MANAGER;
     }
-    if (ksu_late_loaded) {
-        cmd.flags |= KSU_GET_INFO_FLAG_LATE_LOAD;
-    }
 #ifdef EXPECTED_SIZE2
     cmd.flags |= KSU_GET_INFO_FLAG_PR_BUILD;
 #endif
