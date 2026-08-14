tiann/KernelSU

manager: extend navigation badge to superuser and rework home cards

manager/app/src/main/java/me/weishu/kernelsu/ui/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -48,11 +49,15 @@ import androidx.navigation3.ui.NavDisplay
 import androidx.navigationevent.NavigationEventInfo
 import androidx.navigationevent.compose.NavigationBackHandler
 import androidx.navigationevent.compose.rememberNavigationEventState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import me.weishu.kernelsu.Natives
 import me.weishu.kernelsu.ui.component.bottombar.BottomBar
 import me.weishu.kernelsu.ui.component.bottombar.MainPagerState
-import me.weishu.kernelsu.ui.component.bottombar.ModuleBadgeState
+import me.weishu.kernelsu.ui.component.bottombar.NavigationBadgeState
 import me.weishu.kernelsu.ui.component.bottombar.SideRail
 import me.weishu.kernelsu.ui.component.bottombar.rememberMainPagerState
 import me.weishu.kernelsu.ui.navigation3.IntentDispatcher
@@ -81,6 +86,7 @@ import me.weishu.kernelsu.ui.theme.LocalEnableBlur
 import me.weishu.kernelsu.ui.theme.LocalEnableFloatingBottomBar
 import me.weishu.kernelsu.ui.theme.LocalEnableFloatingBottomBarBlur
 import me.weishu.kernelsu.ui.theme.LocalEnableNavigationBadge
+import me.weishu.kernelsu.ui.util.getSuperuserCount
 import me.weishu.kernelsu.ui.util.install
 import me.weishu.kernelsu.ui.util.rememberBlurBackdrop
 import me.weishu.kernelsu.ui.util.rememberContentReady
@@ -88,6 +94,7 @@ import me.weishu.kernelsu.ui.util.rootAvailable
 import me.weishu.kernelsu.ui.viewmodel.MainActivityViewModel
 import me.weishu.kernelsu.ui.viewmodel.MainPagerConfig
 import me.weishu.kernelsu.ui.viewmodel.ModuleViewModel
+import me.weishu.kernelsu.ui.viewmodel.SuperUserViewModel
 import top.yukonga.miuix.kmp.basic.Scaffold
 import top.yukonga.miuix.kmp.blur.layerBackdrop
 import top.yukonga.miuix.kmp.blur.rememberLayerBackdrop
@@ -233,24 +240,39 @@ fun MainScreen(
     var userScrollEnabled by remember(isFullFeatured) { mutableStateOf(isFullFeatured) }
 
     val enableNavigationBadge = LocalEnableNavigationBadge.current
+    val badgeEnabled = enableNavigationBadge && isFullFeatured
     val moduleViewModel = viewModel<ModuleViewModel>()
     val moduleUiState by moduleViewModel.uiState.collectAsStateWithLifecycle()
-    val moduleBadge = if (enableNavigationBadge && isFullFeatured) {
-        ModuleBadgeState(
-            enabledCount = moduleUiState.modules.count { it.enabled },
-            updatableCount = moduleUiState.updateInfo.count { it.value.downloadUrl.isNotBlank() },
-        )
-    } else {
-        ModuleBadgeState()
-    }
-    LaunchedEffect(enableNavigationBadge, isFullFeatured) {
+    LaunchedEffect(badgeEnabled) {
         // The module list normally loads when the module pager is first visited; load it eagerly
         // so the badge is populated while the user is still on another tab.
-        if (enableNavigationBadge && isFullFeatured && moduleViewModel.uiState.value.modules.isEmpty()) {
+        if (badgeEnabled && moduleViewModel.uiState.value.modules.isEmpty()) {
             moduleViewModel.initializePreferences()
             moduleViewModel.loadModuleList()
             moduleViewModel.syncModuleUpdateInfo(moduleViewModel.uiState.value.modules)
         }
+    }
+
+    // Loading the app list just for a badge is too expensive; read the kernel allowlist instead.
+    val superUserViewModel = viewModel<SuperUserViewModel>()
+    val grantedUidCount by remember(superUserViewModel) {
+        superUserViewModel.uiState
+            .map { state -> state.groupedApps.count { it.anyAllowSu } }
+            .distinctUntilChanged()
+    }.collectAsStateWithLifecycle(0)
+    var superuserCount by remember { mutableIntStateOf(0) }
+    LaunchedEffect(badgeEnabled, grantedUidCount) {
+        superuserCount = if (badgeEnabled) withContext(Dispatchers.IO) { getSuperuserCount() } else 0
+    }
+
+    val navigationBadge = if (badgeEnabled) {
+        NavigationBadgeState(
+            superuserCount = superuserCount,
+            moduleEnabledCount = moduleUiState.modules.count { it.enabled },
+            moduleUpdatableCount = moduleUiState.updateInfo.count { it.value.downloadUrl.isNotBlank() },
+        )
+    } else {
+        NavigationBadgeState()
     }
     val uiMode = LocalUiMode.current
     val surfaceColor = when (uiMode) {
@@ -318,7 +340,7 @@ fun MainScreen(
                     containerColor = MaterialTheme.colorScheme.surfaceContainer
                 ) {
                     Row {
-                        SideRail(moduleBadge)
+                        SideRail(navigationBadge)
                         Box(
                             modifier = Modifier
                                 .weight(1f)
@@ -331,7 +353,7 @@ fun MainScreen(
 
                 UiMode.Miuix -> Scaffold { _ ->
                     Row {
-                        SideRail(moduleBadge)
+                        SideRail(navigationBadge)
                         Box(
                             modifier = Modifier
                                 .weight(1f)
@@ -350,7 +372,7 @@ fun MainScreen(
                     BottomBar(
                         blurBackdrop = blurBackdrop,
                         backdrop = backdrop,
-                        moduleBadge = moduleBadge,
+                        navigationBadge = navigationBadge,
                         modifier = Modifier.align(Alignment.BottomCenter),
                     )
                 }

manager/app/src/main/java/me/weishu/kernelsu/ui/component/miuix/WarningCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import me.weishu.kernelsu.ui.component.WarningLevel
 import me.weishu.kernelsu.ui.theme.isInDarkTheme
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CardDefaults
@@ -18,12 +19,11 @@ import top.yukonga.miuix.kmp.theme.MiuixTheme.colorScheme
 import top.yukonga.miuix.kmp.theme.MiuixTheme.isDynamicColor
 import top.yukonga.miuix.kmp.utils.PressFeedbackType
 
-
 @Composable
 fun WarningCard(
     message: String,
     modifier: Modifier = Modifier,
-    color: Color? = null,
+    level: WarningLevel = WarningLevel.Error,
     onClick: (() -> Unit)? = null,
     action: (@Composable () -> Unit)? = null,
 ) {
@@ -31,14 +31,11 @@ fun WarningCard(
         modifier = modifier,
         onClick = { onClick?.invoke() },
         colors = CardDefaults.defaultColors(
-            color = color ?: when {
-                isDynamicColor -> colorScheme.errorContainer
-                isInDarkTheme() -> colorScheme.errorContainer.copy(alpha = 0.3f)
-                else -> colorScheme.errorContainer
-            }
+            color = level.containerColor(),
+            contentColor = level.contentColor(),
         ),
         showIndication = onClick != null,
-        pressFeedbackType = PressFeedbackType.Tilt
+        pressFeedbackType = PressFeedbackType.Sink
     ) {
         Row(
             modifier = Modifier
@@ -49,10 +46,40 @@ fun WarningCard(
         ) {
             Text(
                 text = message,
-                color = if (isDynamicColor) colorScheme.onErrorContainer else colorScheme.error,
                 fontSize = 14.sp
             )
             action?.invoke()
         }
+    }
+}
+
+@Composable
+private fun WarningLevel.containerColor(): Color = when {
+    isDynamicColor -> when (this) {
+        WarningLevel.Error -> colorScheme.errorContainer
+        WarningLevel.Notice -> colorScheme.tertiaryContainer
+    }
+
+    isInDarkTheme() -> when (this) {
+        WarningLevel.Error -> Color(0xFF310808)
+        WarningLevel.Notice -> Color(0xFF3E2F1B)
+    }
+
+    else -> when (this) {
+        WarningLevel.Error -> Color(0xFFF8E2E2)
+        WarningLevel.Notice -> Color(0xFFFFF0DB)
+    }
+}
+
+@Composable
+private fun WarningLevel.contentColor(): Color = when {
+    isDynamicColor -> when (this) {
+        WarningLevel.Error -> colorScheme.onErrorContainer
+        WarningLevel.Notice -> colorScheme.onTertiaryContainer
+    }
+
+    else -> when (this) {
+        WarningLevel.Error -> Color(0xFFF72727)
+        WarningLevel.Notice -> Color(0xFFF5A623)
     }
 }

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -50,11 +51,15 @@ import androidx.navigation3.ui.NavDisplay
 import androidx.navigationevent.NavigationEventInfo
 import androidx.navigationevent.compose.NavigationBackHandler
 import androidx.navigationevent.compose.rememberNavigationEventState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import com.sukisu.ultra.Natives
 import com.sukisu.ultra.ui.component.bottombar.BottomBar
 import com.sukisu.ultra.ui.component.bottombar.MainPagerState
-import com.sukisu.ultra.ui.component.bottombar.ModuleBadgeState
+import com.sukisu.ultra.ui.component.bottombar.NavigationBadgeState
 import com.sukisu.ultra.ui.component.bottombar.SideRail
 import com.sukisu.ultra.ui.component.bottombar.rememberMainPagerState
 import com.sukisu.ultra.ui.kernelFlash.KernelFlashScreen
@@ -89,6 +94,7 @@ import com.sukisu.ultra.ui.theme.LocalEnableBlur
 import com.sukisu.ultra.ui.theme.LocalEnableFloatingBottomBar
 import com.sukisu.ultra.ui.theme.LocalEnableFloatingBottomBarBlur
 import com.sukisu.ultra.ui.theme.LocalEnableNavigationBadge
+import com.sukisu.ultra.ui.util.getSuperuserCount
 import com.sukisu.ultra.ui.util.install
 import com.sukisu.ultra.ui.util.rememberBlurBackdrop
 import com.sukisu.ultra.ui.util.rememberContentReady
@@ -96,6 +102,7 @@ import com.sukisu.ultra.ui.util.rootAvailable
 import com.sukisu.ultra.ui.viewmodel.MainActivityViewModel
 import com.sukisu.ultra.ui.viewmodel.MainPagerConfig
 import com.sukisu.ultra.ui.viewmodel.ModuleViewModel
+import com.sukisu.ultra.ui.viewmodel.SuperUserViewModel
 import top.yukonga.miuix.kmp.basic.Scaffold
 import top.yukonga.miuix.kmp.blur.layerBackdrop
 import top.yukonga.miuix.kmp.blur.rememberLayerBackdrop
@@ -253,24 +260,39 @@ fun MainScreen(
     var userScrollEnabled by remember(isFullFeatured) { mutableStateOf(isFullFeatured) }
 
     val enableNavigationBadge = LocalEnableNavigationBadge.current
+    val badgeEnabled = enableNavigationBadge && isFullFeatured
     val moduleViewModel = viewModel<ModuleViewModel>()
     val moduleUiState by moduleViewModel.uiState.collectAsStateWithLifecycle()
-    val moduleBadge = if (enableNavigationBadge && isFullFeatured) {
-        ModuleBadgeState(
-            enabledCount = moduleUiState.modules.count { it.enabled },
-            updatableCount = moduleUiState.updateInfo.count { it.value.downloadUrl.isNotBlank() },
-        )
-    } else {
-        ModuleBadgeState()
-    }
-    LaunchedEffect(enableNavigationBadge, isFullFeatured) {
+    LaunchedEffect(badgeEnabled) {
         // The module list normally loads when the module pager is first visited; load it eagerly
         // so the badge is populated while the user is still on another tab.
-        if (enableNavigationBadge && isFullFeatured && moduleViewModel.uiState.value.modules.isEmpty()) {
+        if (badgeEnabled && moduleViewModel.uiState.value.modules.isEmpty()) {
             moduleViewModel.initializePreferences()
             moduleViewModel.loadModuleList()
             moduleViewModel.syncModuleUpdateInfo(moduleViewModel.uiState.value.modules)
         }
+    }
+
+    // Loading the app list just for a badge is too expensive; read the kernel allowlist instead.
+    val superUserViewModel = viewModel<SuperUserViewModel>()
+    val grantedUidCount by remember(superUserViewModel) {
+        superUserViewModel.uiState
+            .map { state -> state.groupedApps.count { it.anyAllowSu } }
+            .distinctUntilChanged()
+    }.collectAsStateWithLifecycle(0)
+    var superuserCount by remember { mutableIntStateOf(0) }
+    LaunchedEffect(badgeEnabled, grantedUidCount) {
+        superuserCount = if (badgeEnabled) withContext(Dispatchers.IO) { getSuperuserCount() } else 0
+    }
+
+    val navigationBadge = if (badgeEnabled) {
+        NavigationBadgeState(
+            superuserCount = superuserCount,
+            moduleEnabledCount = moduleUiState.modules.count { it.enabled },
+            moduleUpdatableCount = moduleUiState.updateInfo.count { it.value.downloadUrl.isNotBlank() },
+        )
+    } else {
+        NavigationBadgeState()
     }
     val uiMode = LocalUiMode.current
     val surfaceColor = when (uiMode) {
@@ -338,7 +360,7 @@ fun MainScreen(
                     containerColor = MaterialTheme.colorScheme.surfaceContainer
                 ) {
                     Row {
-                        SideRail(moduleBadge)
+                        SideRail(navigationBadge)
                         Box(
                             modifier = Modifier
                                 .weight(1f)
@@ -351,7 +373,7 @@ fun MainScreen(
 
                 UiMode.Miuix -> Scaffold { _ ->
                     Row {
-                        SideRail(moduleBadge)
+                        SideRail(navigationBadge)
                         Box(
                             modifier = Modifier
                                 .weight(1f)
@@ -370,7 +392,7 @@ fun MainScreen(
                     BottomBar(
                         blurBackdrop = blurBackdrop,
                         backdrop = backdrop,
-                        moduleBadge = moduleBadge,
+                        navigationBadge = navigationBadge,
                         modifier = Modifier.align(Alignment.BottomCenter),
                     )
                 }

manager/app/src/main/java/com/sukisu/ultra/ui/component/miuix/WarningCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.sukisu.ultra.ui.component.WarningLevel
 import com.sukisu.ultra.ui.theme.isInDarkTheme
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.CardDefaults
@@ -18,12 +19,11 @@ import top.yukonga.miuix.kmp.theme.MiuixTheme.colorScheme
 import top.yukonga.miuix.kmp.theme.MiuixTheme.isDynamicColor
 import top.yukonga.miuix.kmp.utils.PressFeedbackType
 
-
 @Composable
 fun WarningCard(
     message: String,
     modifier: Modifier = Modifier,
-    color: Color? = null,
+    level: WarningLevel = WarningLevel.Error,
     onClick: (() -> Unit)? = null,
     action: (@Composable () -> Unit)? = null,
 ) {
@@ -31,14 +31,11 @@ fun WarningCard(
         modifier = modifier,
         onClick = { onClick?.invoke() },
         colors = CardDefaults.defaultColors(
-            color = color ?: when {
-                isDynamicColor -> colorScheme.errorContainer
-                isInDarkTheme() -> colorScheme.errorContainer.copy(alpha = 0.3f)
-                else -> colorScheme.errorContainer
-            }
+            color = level.containerColor(),
+            contentColor = level.contentColor(),
         ),
         showIndication = onClick != null,
-        pressFeedbackType = PressFeedbackType.Tilt
+        pressFeedbackType = PressFeedbackType.Sink
     ) {
         Row(
             modifier = Modifier
@@ -49,10 +46,40 @@ fun WarningCard(
         ) {
             Text(
                 text = message,
-                color = if (isDynamicColor) colorScheme.onErrorContainer else colorScheme.error,
                 fontSize = 14.sp
             )
             action?.invoke()
         }
+    }
+}
+
+@Composable
+private fun WarningLevel.containerColor(): Color = when {
+    isDynamicColor -> when (this) {
+        WarningLevel.Error -> colorScheme.errorContainer
+        WarningLevel.Notice -> colorScheme.tertiaryContainer
+    }
+
+    isInDarkTheme() -> when (this) {
+        WarningLevel.Error -> Color(0xFF310808)
+        WarningLevel.Notice -> Color(0xFF3E2F1B)
+    }
+
+    else -> when (this) {
+        WarningLevel.Error -> Color(0xFFF8E2E2)
+        WarningLevel.Notice -> Color(0xFFFFF0DB)
+    }
+}
+
+@Composable
+private fun WarningLevel.contentColor(): Color = when {
+    isDynamicColor -> when (this) {
+        WarningLevel.Error -> colorScheme.onErrorContainer
+        WarningLevel.Notice -> colorScheme.onTertiaryContainer
+    }
+
+    else -> when (this) {
+        WarningLevel.Error -> Color(0xFFF72727)
+        WarningLevel.Notice -> Color(0xFFF5A623)
     }
 }