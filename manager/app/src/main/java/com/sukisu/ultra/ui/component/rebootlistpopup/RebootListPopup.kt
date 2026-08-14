tiann/KernelSU

manager: guard reboots in jailbreak mode

manager/app/src/main/java/me/weishu/kernelsu/ui/component/rebootlistpopup/RebootListPopup.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.os.PowerManager
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import me.weishu.kernelsu.Natives
 import me.weishu.kernelsu.R
 import me.weishu.kernelsu.ui.LocalUiMode
 import me.weishu.kernelsu.ui.UiMode
+import me.weishu.kernelsu.ui.component.dialog.rememberConfirmDialog
+import me.weishu.kernelsu.ui.util.reboot
 
 data class RebootListOption(
     @param:StringRes val labelRes: Int,
@@ -31,6 +36,24 @@ fun getRebootListOption(): List<RebootListOption> {
         add(RebootListOption(R.string.reboot_bootloader, "bootloader"))
         add(RebootListOption(R.string.reboot_download, "download"))
         add(RebootListOption(R.string.reboot_edl, "edl"))
+    }
+}
+
+/** Reboots on selection, but confirms first in jailbreak mode where a plain reboot drops root. */
+@Composable
+fun rememberRebootAction(): (String) -> Unit {
+    val title = stringResource(R.string.reboot)
+    val message = stringResource(R.string.jailbreak_reboot_warning)
+    val confirmDialog = rememberConfirmDialog(onConfirm = { reboot() })
+
+    return remember(title, message, confirmDialog) {
+        { reason ->
+            if (Natives.isLateLoadMode && reason.isEmpty()) {
+                confirmDialog.showConfirm(title = title, content = message)
+            } else {
+                reboot(reason)
+            }
+        }
     }
 }
 

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/component/rebootlistpopup/RebootListPopup.kt
@@ -4,10 +4,15 @@ import android.content.Context
 import android.os.PowerManager
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.sukisu.ultra.Natives
 import com.sukisu.ultra.R
 import com.sukisu.ultra.ui.LocalUiMode
 import com.sukisu.ultra.ui.UiMode
+import com.sukisu.ultra.ui.component.dialog.rememberConfirmDialog
+import com.sukisu.ultra.ui.util.reboot
 
 data class RebootListOption(
     @param:StringRes val labelRes: Int,
@@ -31,6 +36,24 @@ fun getRebootListOption(): List<RebootListOption> {
         add(RebootListOption(R.string.reboot_bootloader, "bootloader"))
         add(RebootListOption(R.string.reboot_download, "download"))
         add(RebootListOption(R.string.reboot_edl, "edl"))
+    }
+}
+
+/** Reboots on selection, but confirms first in jailbreak mode where a plain reboot drops root. */
+@Composable
+fun rememberRebootAction(): (String) -> Unit {
+    val title = stringResource(R.string.reboot)
+    val message = stringResource(R.string.jailbreak_reboot_warning)
+    val confirmDialog = rememberConfirmDialog(onConfirm = { reboot() })
+
+    return remember(title, message, confirmDialog) {
+        { reason ->
+            if (Natives.isLateLoadMode && reason.isEmpty()) {
+                confirmDialog.showConfirm(title = title, content = message)
+            } else {
+                reboot(reason)
+            }
+        }
     }
 }
 