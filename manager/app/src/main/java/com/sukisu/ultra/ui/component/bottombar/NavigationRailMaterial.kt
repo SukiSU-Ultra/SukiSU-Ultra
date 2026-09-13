tiann/KernelSU

manager: persist NavRail expansion state

manager/app/src/main/java/me/weishu/kernelsu/ui/component/bottombar/NavigationRailMaterial.kt
@@ -30,6 +30,8 @@ import androidx.compose.material3.WideNavigationRailItem
 import androidx.compose.material3.WideNavigationRailValue
 import androidx.compose.material3.rememberWideNavigationRailState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -37,6 +39,7 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import me.weishu.kernelsu.Natives
 import me.weishu.kernelsu.R
+import me.weishu.kernelsu.data.repository.SettingsRepositoryImpl
 import me.weishu.kernelsu.ui.LocalMainPagerState
 import me.weishu.kernelsu.ui.util.rootAvailable
 
@@ -58,9 +61,20 @@ fun NavigationRailMaterial(
         Triple(R.string.settings, Icons.Filled.Settings, Icons.Outlined.Settings)
     )
 
-    val state = rememberWideNavigationRailState()
+    val settingsRepo = remember { SettingsRepositoryImpl() }
+    val state = rememberWideNavigationRailState(
+        initialValue = if (settingsRepo.navigationRailExpanded) {
+            WideNavigationRailValue.Expanded
+        } else {
+            WideNavigationRailValue.Collapsed
+        },
+    )
     val scope = rememberCoroutineScope()
     val expanded = state.targetValue == WideNavigationRailValue.Expanded
+    LaunchedEffect(state.targetValue) {
+        settingsRepo.navigationRailExpanded =
+            state.targetValue == WideNavigationRailValue.Expanded
+    }
 
     WideNavigationRail(
         modifier = modifier.fillMaxHeight(),

manager/app/src/main/java/me/weishu/kernelsu/ui/component/bottombar/NavigationRailMiuix.kt
@@ -1,14 +1,18 @@
 package me.weishu.kernelsu.ui.component.bottombar
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import me.weishu.kernelsu.Natives
 import me.weishu.kernelsu.R
+import me.weishu.kernelsu.data.repository.SettingsRepositoryImpl
 import me.weishu.kernelsu.ui.LocalMainPagerState
 import me.weishu.kernelsu.ui.util.rootAvailable
 import top.yukonga.miuix.kmp.basic.NavigationRail
 import top.yukonga.miuix.kmp.basic.NavigationRailItem
+import top.yukonga.miuix.kmp.basic.NavigationRailValue
 import top.yukonga.miuix.kmp.basic.rememberNavigationRailState
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 
@@ -26,10 +30,21 @@ fun NavigationRailMiuix(
     val items = BottomBarDestination.entries.map { destination ->
         Pair(stringResource(destination.label), destination.icon)
     }
+    val settingsRepo = remember { SettingsRepositoryImpl() }
+    val state = rememberNavigationRailState(
+        initialValue = if (settingsRepo.navigationRailExpanded) {
+            NavigationRailValue.Expanded
+        } else {
+            NavigationRailValue.Collapsed
+        },
+    )
+    LaunchedEffect(state.currentValue) {
+        settingsRepo.navigationRailExpanded = state.isExpanded
+    }
 
     NavigationRail(
         modifier = modifier,
-        state = rememberNavigationRailState(),
+        state = state,
         color = MiuixTheme.colorScheme.surface,
         expandContentDescription = stringResource(R.string.nav_rail_expand),
         collapseContentDescription = stringResource(R.string.nav_rail_collapse),

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/component/bottombar/NavigationRailMaterial.kt
@@ -30,6 +30,8 @@ import androidx.compose.material3.WideNavigationRailItem
 import androidx.compose.material3.WideNavigationRailValue
 import androidx.compose.material3.rememberWideNavigationRailState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -37,6 +39,7 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import com.sukisu.ultra.Natives
 import com.sukisu.ultra.R
+import com.sukisu.ultra.data.repository.SettingsRepositoryImpl
 import com.sukisu.ultra.ui.LocalMainPagerState
 import com.sukisu.ultra.ui.util.rootAvailable
 
@@ -58,9 +61,20 @@ fun NavigationRailMaterial(
         Triple(R.string.settings, Icons.Filled.Settings, Icons.Outlined.Settings)
     )
 
-    val state = rememberWideNavigationRailState()
+    val settingsRepo = remember { SettingsRepositoryImpl() }
+    val state = rememberWideNavigationRailState(
+        initialValue = if (settingsRepo.navigationRailExpanded) {
+            WideNavigationRailValue.Expanded
+        } else {
+            WideNavigationRailValue.Collapsed
+        },
+    )
     val scope = rememberCoroutineScope()
     val expanded = state.targetValue == WideNavigationRailValue.Expanded
+    LaunchedEffect(state.targetValue) {
+        settingsRepo.navigationRailExpanded =
+            state.targetValue == WideNavigationRailValue.Expanded
+    }
 
     WideNavigationRail(
         modifier = modifier.fillMaxHeight(),

manager/app/src/main/java/com/sukisu/ultra/ui/component/bottombar/NavigationRailMiuix.kt
@@ -1,14 +1,18 @@
 package com.sukisu.ultra.ui.component.bottombar
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.sukisu.ultra.Natives
 import com.sukisu.ultra.R
+import com.sukisu.ultra.data.repository.SettingsRepositoryImpl
 import com.sukisu.ultra.ui.LocalMainPagerState
 import com.sukisu.ultra.ui.util.rootAvailable
 import top.yukonga.miuix.kmp.basic.NavigationRail
 import top.yukonga.miuix.kmp.basic.NavigationRailItem
+import top.yukonga.miuix.kmp.basic.NavigationRailValue
 import top.yukonga.miuix.kmp.basic.rememberNavigationRailState
 import top.yukonga.miuix.kmp.theme.MiuixTheme
 
@@ -26,10 +30,21 @@ fun NavigationRailMiuix(
     val items = BottomBarDestination.entries.map { destination ->
         Pair(stringResource(destination.label), destination.icon)
     }
+    val settingsRepo = remember { SettingsRepositoryImpl() }
+    val state = rememberNavigationRailState(
+        initialValue = if (settingsRepo.navigationRailExpanded) {
+            NavigationRailValue.Expanded
+        } else {
+            NavigationRailValue.Collapsed
+        },
+    )
+    LaunchedEffect(state.currentValue) {
+        settingsRepo.navigationRailExpanded = state.isExpanded
+    }
 
     NavigationRail(
         modifier = modifier,
-        state = rememberNavigationRailState(),
+        state = state,
         color = MiuixTheme.colorScheme.surface,
         expandContentDescription = stringResource(R.string.nav_rail_expand),
         collapseContentDescription = stringResource(R.string.nav_rail_collapse),