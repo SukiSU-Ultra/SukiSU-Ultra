tiann/KernelSU

fix(manager): Use MediaStore to download modules (#3707)

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -110,6 +110,7 @@ import me.weishu.kernelsu.ui.component.material.TopBarBackButton
 import me.weishu.kernelsu.ui.component.material.expressiveTopAppBarColors
 import me.weishu.kernelsu.ui.component.statustag.StatusTag
 import me.weishu.kernelsu.ui.util.download
+import me.weishu.kernelsu.ui.util.isDownloadAvailable
 import me.weishu.kernelsu.ui.util.rememberContentReady
 
 @SuppressLint("LocalContextGetResourceValueCall")
@@ -707,6 +708,7 @@ private fun ReleaseAssetSegmentedItem(
                         onDownloading = { isDownloading = true },
                         onProgress = { p -> scope.launch(Dispatchers.Main) { progress = p } }
                     )
+                    isDownloading = false
                 }
             }
             confirmDialog.showConfirm(title = confirmTitle, content = startText)
@@ -721,11 +723,12 @@ private fun ReleaseAssetSegmentedItem(
                 FilledTonalButton(
                     onClick = {
                         val uri = downloadedUri ?: return@FilledTonalButton
-                        val file = uri.path?.let { java.io.File(it) }
-                        if (file != null && file.exists()) {
-                            onInstallModule(uri)
-                        } else {
-                            downloadedUri = null
+                        scope.launch {
+                            if (isDownloadAvailable(uri)) {
+                                onInstallModule(uri)
+                            } else {
+                                downloadedUri = null
+                            }
                         }
                     },
                     contentPadding = ButtonDefaults.TextButtonContentPadding

manager/app/src/main/java/me/weishu/kernelsu/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -90,6 +90,7 @@ import me.weishu.kernelsu.ui.theme.LocalEnableBlur
 import me.weishu.kernelsu.ui.theme.isInDarkTheme
 import me.weishu.kernelsu.ui.util.BlurredBar
 import me.weishu.kernelsu.ui.util.download
+import me.weishu.kernelsu.ui.util.isDownloadAvailable
 import me.weishu.kernelsu.ui.util.rememberBlurBackdrop
 import me.weishu.kernelsu.ui.util.rememberContentReady
 import top.yukonga.miuix.kmp.basic.Card
@@ -781,6 +782,7 @@ fun ReleasesPage(
                                                             },
                                                             onDownloading = { isDownloading = true },
                                                             onProgress = { p -> scope.launch(Dispatchers.Main) { progress = p } })
+                                                        isDownloading = false
                                                     }
                                                 }
                                                 confirmDialog.showConfirm(title = confirmTitle, content = startText)
@@ -815,11 +817,12 @@ fun ReleasesPage(
                                                     minWidth = 35.dp,
                                                     onClick = {
                                                         val uri = downloadedUri ?: return@IconButton
-                                                        val file = uri.path?.let { java.io.File(it) }
-                                                        if (file != null && file.exists()) {
-                                                            onInstallModule(uri)
-                                                        } else {
-                                                            downloadedUri = null
+                                                        scope.launch {
+                                                            if (isDownloadAvailable(uri)) {
+                                                                onInstallModule(uri)
+                                                            } else {
+                                                                downloadedUri = null
+                                                            }
                                                         }
                                                     },
                                                 ) {

SukiSU-Ultra/SukiSU-Ultra

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMaterial.kt
@@ -110,6 +110,7 @@ import com.sukisu.ultra.ui.component.material.TopBarBackButton
 import com.sukisu.ultra.ui.component.material.expressiveTopAppBarColors
 import com.sukisu.ultra.ui.component.statustag.StatusTag
 import com.sukisu.ultra.ui.util.download
+import com.sukisu.ultra.ui.util.isDownloadAvailable
 import com.sukisu.ultra.ui.util.rememberContentReady
 
 @SuppressLint("LocalContextGetResourceValueCall")
@@ -719,6 +720,7 @@ private fun ReleaseAssetSegmentedItem(
                         onDownloading = { isDownloading = true },
                         onProgress = { p -> scope.launch(Dispatchers.Main) { progress = p } }
                     )
+                    isDownloading = false
                 }
             }
             confirmDialog.showConfirm(title = confirmTitle, content = startText)
@@ -733,11 +735,12 @@ private fun ReleaseAssetSegmentedItem(
                 FilledTonalButton(
                     onClick = {
                         val uri = downloadedUri ?: return@FilledTonalButton
-                        val file = uri.path?.let { java.io.File(it) }
-                        if (file != null && file.exists()) {
-                            onInstallModule(uri)
-                        } else {
-                            downloadedUri = null
+                        scope.launch {
+                            if (isDownloadAvailable(uri)) {
+                                onInstallModule(uri)
+                            } else {
+                                downloadedUri = null
+                            }
                         }
                     },
                     contentPadding = ButtonDefaults.TextButtonContentPadding

manager/app/src/main/java/com/sukisu/ultra/ui/screen/modulerepo/ModuleRepoMiuix.kt
@@ -90,6 +90,7 @@ import com.sukisu.ultra.ui.theme.LocalEnableBlur
 import com.sukisu.ultra.ui.theme.isInDarkTheme
 import com.sukisu.ultra.ui.util.BlurredBar
 import com.sukisu.ultra.ui.util.download
+import com.sukisu.ultra.ui.util.isDownloadAvailable
 import com.sukisu.ultra.ui.util.rememberBlurBackdrop
 import com.sukisu.ultra.ui.util.rememberContentReady
 import top.yukonga.miuix.kmp.basic.Card
@@ -809,6 +810,7 @@ fun ReleasesPage(
                                                             },
                                                             onDownloading = { isDownloading = true },
                                                             onProgress = { p -> scope.launch(Dispatchers.Main) { progress = p } })
+                                                        isDownloading = false
                                                     }
                                                 }
                                                 confirmDialog.showConfirm(title = confirmTitle, content = startText)
@@ -843,11 +845,12 @@ fun ReleasesPage(
                                                     minWidth = 35.dp,
                                                     onClick = {
                                                         val uri = downloadedUri ?: return@IconButton
-                                                        val file = uri.path?.let { java.io.File(it) }
-                                                        if (file != null && file.exists()) {
-                                                            onInstallModule(uri)
-                                                        } else {
-                                                            downloadedUri = null
+                                                        scope.launch {
+                                                            if (isDownloadAvailable(uri)) {
+                                                                onInstallModule(uri)
+                                                            } else {
+                                                                downloadedUri = null
+                                                            }
                                                         }
                                                     },
                                                 ) {